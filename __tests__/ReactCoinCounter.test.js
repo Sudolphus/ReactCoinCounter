812a74dd9c05e00128566b02340367499a079fe4
Cody Fritz <clanalia55@gmail.com>
2020-08-31 14:01:03 -0700

    update progress with passing and functional tests
    Co-authored-by: Sudolphus <micheal.hansen@gmail.com>
    Co-authored-by: jkbutserious <kim2jy@hotmail.com>

--- __tests__/ReactCoinCounter.test.js
+++ __tests__/ReactCoinCounter.test.js
@@ -13,5 +13,30 @@ describe("CoinCounter", () =>{
   test("Should know the value of a dime", () => {
     const dimes = CoinCounter(.10);
     expect(dimes).toBe("1 dimes");
+  });
+
+  test("Should know the value of a nickel", ()=>{
+    const nickels = CoinCounter(.05);
+    expect(nickels).toBe("1 nickels");
+  });
+
+  test("should know the value of a penny", ()=>{
+    const pennies = CoinCounter(0.01);
+    expect(pennies).toBe("1 pennies");
+  });
+  
+  test('should know how many quarters it needs', () =>{
+    const value = 4.75;
+    expect(CoinCounter(value)).toBe("19 quarters");
+  });
+
+  test('should know how many additional dimes it needs', () => {
+    const value = 4.85;
+    expect(CoinCounter(value)).toBe("19 quarters 1 dimes");
+  });
+
+  test('shoudl know how many additional nickels it needs', () => {
+    const value = 4.90;
+    expect(CoinCounter(value)).toBe("19 quarters 1 dimes 1 nickels");
   })
 });
--- src/ReactCoinCounter.js
+++ src/ReactCoinCounter.js
@@ -1,10 +1,23 @@
+const round = function(num) {
+  return((Math.round(num*100)/100).toFixed(2));
+}
+
 export function CoinCounter(value) {
   if (value >= 0.25) {
     const numberOfQuarters = Math.floor(value / .25);
-    return numberOfQuarters.toString() + " quarters";    
-  }
-  else if (value >= 0.10) {
+    value -= (numberOfQuarters * 0.25);
+    return (numberOfQuarters.toString() + " quarters" + " " + CoinCounter(round(value))).trim();
+  } else if (value >= 0.10) {
     const numberOfDimes = Math.floor(value / 0.10);
-    return numberOfDimes.toString() + " dimes";
+    value -= (numberOfDimes * 0.10);
+    return (numberOfDimes.toString() + " dimes" + " " + CoinCounter(round(value))).trim();
+  } else if (value >= 0.05){
+    const numberOfNickels = Math.floor(value / 0.05);
+    return numberOfNickels.toString() + " nickels";
+  } else if (value >= 0.01){
+    const numberOfPennies = Math.floor(value / 0.01);
+    return numberOfPennies.toString() + " pennies";
+  } else {
+    return "";
   }
 }
