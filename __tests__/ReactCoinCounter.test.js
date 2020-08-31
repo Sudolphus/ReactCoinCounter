499f06bbc546c9811896d9fe8a603c36838d9272
Cody Fritz <clanalia55@gmail.com>
2020-08-31 14:29:34 -0700

    practice currying and recursive functions
    Co-authored-by: Sudolphus <micheal.hansen@gmail.com>
    Co-authored-by: jkbutserious <kim2jy@hotmail.com>

--- __tests__/ReactCoinCounter.test.js
+++ __tests__/ReactCoinCounter.test.js
@@ -1,42 +1,57 @@
 /* eslint-disable no-undef */
-import { CoinCounter } from './../src/ReactCoinCounter';
+import { CoinCounter, CurryCounter, Coin } from './../src/ReactCoinCounter';
 
 describe("CoinCounter", () =>{
 // Quarter, Dime, Nickel, Penny
 
 
-  test("should know the value of a quarter", ()=>{
-    const quarters = CoinCounter(.25);
-    expect(quarters).toBe("1 quarters");
-  });
+  // test("should know the value of a quarter", ()=>{
+  //   const quarters = CoinCounter(.25);
+  //   expect(quarters).toBe("1 quarters");
+  // });
 
-  test("Should know the value of a dime", () => {
-    const dimes = CoinCounter(.10);
-    expect(dimes).toBe("1 dimes");
-  });
+  // test("Should know the value of a dime", () => {
+  //   const dimes = CoinCounter(.10);
+  //   expect(dimes).toBe("1 dimes");
+  // });
 
-  test("Should know the value of a nickel", ()=>{
-    const nickels = CoinCounter(.05);
-    expect(nickels).toBe("1 nickels");
-  });
+  // test("Should know the value of a nickel", ()=>{
+  //   const nickels = CoinCounter(.05);
+  //   expect(nickels).toBe("1 nickels");
+  // });
 
-  test("should know the value of a penny", ()=>{
-    const pennies = CoinCounter(0.01);
-    expect(pennies).toBe("1 pennies");
-  });
+  // test("should know the value of a penny", ()=>{
+  //   const pennies = CoinCounter(0.01);
+  //   expect(pennies).toBe("1 pennies");
+  // });
   
-  test('should know how many quarters it needs', () =>{
-    const value = 4.75;
-    expect(CoinCounter(value)).toBe("19 quarters");
-  });
+  // test('should know how many quarters it needs', () =>{
+  //   const value = 4.75;
+  //   expect(CoinCounter(value)).toBe("19 quarters");
+  // });
 
-  test('should know how many additional dimes it needs', () => {
-    const value = 4.85;
-    expect(CoinCounter(value)).toBe("19 quarters 1 dimes");
-  });
+  // test('should know how many additional dimes it needs', () => {
+  //   const value = 4.85;
+  //   expect(CoinCounter(value)).toBe("19 quarters 1 dimes");
+  // });
 
-  test('shoudl know how many additional nickels it needs', () => {
-    const value = 4.90;
-    expect(CoinCounter(value)).toBe("19 quarters 1 dimes 1 nickels");
+  // test('shoudl know how many additional nickels it needs', () => {
+  //   const value = 4.90;
+  //   expect(CoinCounter(value)).toBe("19 quarters 1 dimes 1 nickels");
+  // })
+
+  // test("should know how many additional pennies it needs", () =>{
+  //   const value = 4.91;
+  //   expect(CoinCounter(value)).toBe("19 quarters 1 dimes 1 nickels 1 pennies");
+  // })
+
+  test("should know how many quarters are in a given value", ()=>{
+    const quarterCalc = Coin(.25);
+    expect(quarterCalc(4.99)).toBe(19);
+  })
+
+  test("should know how many dimes are in a given value", ()=>{
+    const dimeCalc = Coin(.1);
+    expect(dimeCalc(.19)).toBe(1);
   })
 });
--- src/ReactCoinCounter.js
+++ src/ReactCoinCounter.js
@@ -2,22 +2,65 @@ const round = function(num) {
   return((Math.round(num*100)/100).toFixed(2));
 }
 
-export function CoinCounter(value) {
-  if (value >= 0.25) {
-    const numberOfQuarters = Math.floor(value / .25);
-    value -= (numberOfQuarters * 0.25);
-    return (numberOfQuarters.toString() + " quarters" + " " + CoinCounter(round(value))).trim();
-  } else if (value >= 0.10) {
-    const numberOfDimes = Math.floor(value / 0.10);
-    value -= (numberOfDimes * 0.10);
-    return (numberOfDimes.toString() + " dimes" + " " + CoinCounter(round(value))).trim();
-  } else if (value >= 0.05){
-    const numberOfNickels = Math.floor(value / 0.05);
-    return numberOfNickels.toString() + " nickels";
-  } else if (value >= 0.01){
-    const numberOfPennies = Math.floor(value / 0.01);
-    return numberOfPennies.toString() + " pennies";
-  } else {
-    return "";
+// export function CoinCounter(value) {
+//   if (value >= 0.25) {
+//     const numberOfQuarters = Math.floor(value / .25);
+//     value -= (numberOfQuarters * 0.25);
+//     return (numberOfQuarters.toString() + " quarters" + " " + CoinCounter(round(value))).trim();
+  
+//   } else if (value >= 0.10) {
+//     const numberOfDimes = Math.floor(value / 0.10);
+//     value -= (numberOfDimes * 0.10);
+//     return (numberOfDimes.toString() + " dimes" + " " + CoinCounter(round(value))).trim();
+  
+//   } else if (value >= 0.05){
+//     const numberOfNickels = Math.floor(value / 0.05);
+//     value -= (numberOfNickels * 0.05);
+//     return (numberOfNickels.toString() + " nickels" + " " + CoinCounter(round(value))).trim();
+  
+//   } else if (value >= 0.01){
+//     const numberOfPennies = Math.floor(value / 0.01);
+//     return (numberOfPennies.toString() + " pennies").trim();
+//   } else {
+//     return "";
+//   }
+// }
+
+// const recursiveCount = function(value, quarters = 0, dimes = 0, nickels = 0){
+//   if (value > .25) {
+//     quarters++;
+//     value -= .25;
+//   } else if (value >= .10) {
+//     dimes++;
+//     value -= .1;
+//   } else if (value >= .05) {
+//     nickels++;
+//     value -= .05;
+//   }
+//   if (value >= .05)
+//   {
+//     return recursiveCount(value, quarters, dimes, nickels);
+//   } else {
+//     return `${quarters} quarters, ${dimes} dimes, ${nickels} nickels, ${value*100} pennies`;
+//   }
+// }
+
+export const Coin = function(coinValue) {
+  return (value) => {
+    return Math.floor(value/coinValue);
   }
+}
+
+
+export const CurryCounter = function(value) {
+  const quarterCalc = Coin(.25);
+  const numberOfQuarters = quarterCalc(value);
+  value = round(value-numberOfQuarters*.25);
+  const dimeCalc = Coin(.1);
+  const numberOfDimes = dimeCalc(value);
+  value = round(value-numberOfDimes*.1);
+  const nickelCalc = Coin(.05);
+  const numberOfNickels = nickelCalc(value);
+  value = round(value-numberOfNickels*.05);
+  return `${numberOfQuarters} quarters, ${numberOfDimes} dimes, ${numberOfNickels} nickels, ${Math.floor(value*100)} pennies`;
 }
